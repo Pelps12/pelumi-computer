f82bb6ee89ccaa55caac4941620e6b91c64a6047
Pelps12 <oluwapelps@gmail.com>
2024-11-02 23:10:13 -0500

    Add public url

--- src/routes/api/proxy/+server.ts
+++ src/routes/api/proxy/+server.ts
@@ -1,16 +1,17 @@
+import { env } from '$env/dynamic/private';
 import type { RequestHandler } from '@sveltejs/kit';
 export const GET: RequestHandler = async ({ setHeaders }) => {
-	const response = await fetch(`${'http://localhost:5173'}/index.js`);
+	const response = await fetch(`${env.PUBLIC_URL}/index.js`);
 
 	setHeaders({
 		'cache-control': 'max-age=60'
 	});
-    const headers = new Headers();
-    headers.append('Access-Control-Allow-Methods', 'GET, OPTIONS');
+	const headers = new Headers();
+	headers.append('Access-Control-Allow-Methods', 'GET, OPTIONS');
 	headers.append('Access-Control-Allow-Origin', '*');
 	headers.append('Access-Control-Allow-Headers', '*');
-    headers.append('Content-Type', 'text/javascript');
-    headers.append('Cross-Origin-Embedder-Policy', 'require-corp',)
+	headers.append('Content-Type', 'text/javascript');
+	headers.append('Cross-Origin-Embedder-Policy', 'require-corp');
 	return new Response(await response.blob(), {
 		headers
 	});
